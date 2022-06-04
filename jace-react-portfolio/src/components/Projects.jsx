57e78f084e555f4749306ff07344d21133759a7d
jacef790 <jace.feinstein@live.com>
2022-06-04 00:20:38 -0600

    project page finished

--- jace-react-portfolio/src/components/Projects.jsx
+++ jace-react-portfolio/src/components/Projects.jsx
@@ -18,22 +18,19 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="grid grid-col-1 md:grid-cols-2 gap-4">
-          <div
-            style={{ backgroundImage: `url(${Duck})`}}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+        <div className="grid grid-col-1 md:grid-cols-2 gap-4 bg-[#7ccb57]">
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div className=" ">
+              <span className="text-2xl font-bold text-[#023147] tracking-wider">
+                Quacker, a social media experience
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://glacial-shelf-75917.herokuapp.com/" target='blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/cycoconutz/Project-2-Quacker" target='blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -41,7 +38,70 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div></div>
+          <div>
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div className=" ">
+              <span className="text-2xl font-bold text-[#023147] tracking-wider">
+                Compatibility Finder
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://tim-zebra.github.io/Compatible-or-Not_Find-true-love-here./" target='blank'>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Tim-Zebra/Compatible-or-Not_Find-true-love-here." target='blank'>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          
+          </div>
+          <div>
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div className=" ">
+              <span className="text-2xl font-bold text-[#023147] tracking-wider">
+                Note Taker
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://murmuring-cliffs-95595.herokuapp.com/" target='blank'>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/jacef790/Note-Taker" target='blank'>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div>
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div className=" ">
+              <span className="text-2xl font-bold text-[#023147] tracking-wider">
+                Password Generator
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://jacef790.github.io/HW-03-PasswordGenerator/" target='blank'>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/jacef790/HW-03-PasswordGenerator" target='blank'>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
         </div>
       </div>
     </div>
